66d629d137d84b69492deef4282b6efc6318f6d8
itsprajapati1204 <shubham902647@gmail.com>
2023-11-04 11:34:29 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+let ChangeIcon=function(icon){
+  icon.classList.toggle("fi-ss-moon-stars")
+}
+// let HamIcon=function(icon01){
+//   icon01.classList.toggle("fi-br-cross")
+// }
+
+
+var element;
+
+element=document.getElementById("newsec").innerHTML="<h2>Newweb</h2>";
+
+console.log(element)
